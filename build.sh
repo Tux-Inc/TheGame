040bf40e77bf5a6ad4b0e0d9bd98d02ebeb11d10
Lionel Silva <ld487@kent.ac.uk>
2023-12-01 16:41:47 +0100

    add fecthcontent

--- build.sh
+++ build.sh
@@ -4,11 +4,13 @@ OUT_DIR="./build"
 BUILD_DIR="./build"
 SRC_DIR="./"
 
-if [ -d $OUT_DIR ]; then
-    rm -Rf $OUT_DIR;
-fi
+# if [ -d $OUT_DIR ]; then
+#     rm -Rf $OUT_DIR;
+# fi
 
-mkdir -p $BUILD_DIR
+if [ ! -d $OUT_DIR ]; then
+    mkdir -p $BUILD_DIR
+fi
 
 cmake -S $SRC_DIR -G Ninja  -B $BUILD_DIR
 
