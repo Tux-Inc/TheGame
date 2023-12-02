e873d7d98aaf163e09192780dc840bc291ccc596
Lionel Silva <ld487@kent.ac.uk>
2023-12-02 12:21:54 +0100

    test player

--- src/entities/Player.cpp
+++ src/entities/Player.cpp
@@ -3,12 +3,17 @@
 Player::Player(vec2f scale)
 {
     _scaleFactor = scale;
-    sf::CircleShape *shape = new sf::CircleShape(_scaleFactor.x * 50);
-    shape->setFillColor(sf::Color::Green);
+    sf::Texture *t = new sf::Texture();
+    t->loadFromFile("./assets/img/Root_Walk.png");
+    sf::IntRect rect({ 0, 128, 64, 64 });
+    sf::Sprite *s = new sf::Sprite(*t);
+
     _pos = scaleVector((sf::Vector2f) { 100, 100 }, _scaleFactor);
-    shape->setPosition(_pos);
-    _drawable = shape;
-    _transformable = shape;
+    s->setPosition(_pos);
+    s->setTextureRect(rect);
+    s->setScale((sf::Vector2f) { _scaleFactor.x, _scaleFactor.y });
+    _drawable = s;
+    _transformable = s;
 }
 
 Player::~Player()
