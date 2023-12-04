d6ee4358e42eba2abfd699ba65fa27c212d778dc
AlexandreDFM <alexandre-kevin.de-freitas-martins@epitech.eu>
2023-12-04 02:48:15 +0100

    refactor(windows): try to rework windows compilation

--- build.bat
+++ build.bat
@@ -9,7 +9,7 @@ if not exist %OUT_DIR% (
 )
 
 @REM "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
-cmake -S %SRC_DIR% -G Ninja -B %BUILD_DIR% -DCMAKE_MAKE_PROGRAM="C:\Program Files\Ninja_Build\ninja.exe"
+cmake -S %SRC_DIR% -GNinja -B %BUILD_DIR% -DCMAKE_MAKE_PROGRAM="C:\Program Files\Ninja_Build\ninja.exe"
 
 ninja -C %BUILD_DIR%
 
