9e3724a1dc43ae0261c54ec39fc917551518ac0a
cram0 <j.augugliaro@outlook.fr>
2023-12-06 11:50:06 +0100

    Update map_editor config and assets/maps/map1/layers1 and layers2

--- map_editor/lib/Editor.hpp
+++ map_editor/lib/Editor.hpp
@@ -24,13 +24,13 @@ class Layer {
             _width = width;
             _height = height;
 
-            _tiles = FileToTiles(filePath);
+            _tiles = FileToTiles(filePath, type);
             _texture.loadFromFile("../assets/img/map_futuristique.png");
             if (!_texture.loadFromFile("../assets/img/map_futuristique.png"))
                 std::cout << "Error loading texture" << std::endl;
         }
 
-        std::vector<std::vector<Tile *>> FileToTiles(std::string filePath)
+        std::vector<std::vector<Tile *>> FileToTiles(std::string filePath, Tile::Type type)
         {
             std::vector<std::vector<Tile *>> tiles;
             std::ifstream file(filePath);
@@ -40,9 +40,9 @@ class Layer {
                 while (getline(file, line)) {
                     std::vector<Tile *> row;
                     for (char c : line) {
-                        Tile *tile = new Tile(Tile::Type::FLOOR, new sf::Sprite(), c);
+                        Tile *tile = new Tile(type, new sf::Sprite(), c);
                         tile->GetSprite()->setTexture(_texture, false);
-                        tile->GetSprite()->setTextureRect(sf::IntRect(0, 0, 48, 48));
+                        tile->GetSprite()->setTextureRect(GetRectFromChar(c));
                         tile->GetSprite()->setScale(0.25f, 0.25f);
                         row.push_back(tile);
                     }
@@ -51,6 +51,39 @@ class Layer {
             }
             file.close();
             return tiles;
+        }
+
+        sf::IntRect GetRectFromChar(char c)
+        {
+            std::ifstream file("config.me");
+            if (!file.is_open())
+                std::cerr << "Failed to open config file" << std::endl;
+            std::string line;
+
+            while (std::getline(file, line)) {
+                std::stringstream ss(line);
+                std::string line;
+                std::vector<std::string> tokens;
+                std::string delimiter = " ";
+                std::string token;
+
+                while (std::getline(ss, token, ' '))
+                    tokens.push_back(token);
+
+                if (tokens[0] == "floor" && tokens[3][0] == c)
+                    return sf::IntRect(std::stoi(tokens[1]), std::stoi(tokens[2]), 48, 48);
+                if (tokens[0] == "wall" && tokens[3][0] == c) {
+                    std::cout << "Wall found" << std::endl;
+                    return sf::IntRect(std::stoi(tokens[1]), std::stoi(tokens[2]), 48, 48);
+                }
+                if (tokens[0] == "object" && tokens[3][0] == c)
+                    return sf::IntRect(std::stoi(tokens[1]), std::stoi(tokens[2]), 48, 48);
+
+                tokens.clear();
+            }
+
+            file.close();
+            return sf::IntRect(48, 0, 48, 48);
         }
 
         ~Layer()
@@ -101,9 +134,9 @@ class Map {
         {
             _width = width;
             _height = height;
-            _layers.push_back(new Layer(Tile::FLOOR, _width, _height, "../assets/maps/map1/layers1"));
-            _layers.push_back(new Layer(Tile::WALL, _width, _height, "../assets/maps/map1/layers2"));
-            _layers.push_back(new Layer(Tile::OBJECT, _width, _height, "../assets/maps/map1/layers3"));
+            _layers.push_back(new Layer(Tile::Type::FLOOR, _width, _height, "../assets/maps/map1/layers1"));
+            _layers.push_back(new Layer(Tile::Type::WALL, _width, _height, "../assets/maps/map1/layers2"));
+            _layers.push_back(new Layer(Tile::Type::OBJECT, _width, _height, "../assets/maps/map1/layers3"));
             SetMapPosition(50, 50);
         }
 
@@ -153,6 +186,27 @@ class Map {
                     }
                 }
             }
+
+            // // Draw floor layer
+            // for (size_t y = 0; y < _layers[Tile::Type::FLOOR]->GetHeight(); y++) {
+            //     for (size_t x = 0; x < _layers[Tile::Type::FLOOR]->GetWidth(); x++) {
+            //         window.draw(*_layers[Tile::Type::FLOOR]->GetTile(x, y)->GetSprite());
+            //     }
+            // }
+
+            // // Draw object layer
+            // for (size_t y = 0; y < _layers[Tile::Type::OBJECT]->GetHeight(); y++) {
+            //     for (size_t x = 0; x < _layers[Tile::Type::OBJECT]->GetWidth(); x++) {
+            //         window.draw(*_layers[Tile::Type::OBJECT]->GetTile(x, y)->GetSprite());
+            //     }
+            // }
+
+            // // Draw wall layer
+            // for (size_t y = 0; y < _layers[Tile::Type::WALL]->GetHeight(); y++) {
+            //     for (size_t x = 0; x < _layers[Tile::Type::WALL]->GetWidth(); x++) {
+            //         window.draw(*_layers[Tile::Type::WALL]->GetTile(x, y)->GetSprite());
+            //     }
+            // }
         }
 
         std::vector<Layer *> GetLayers() { return _layers; }
@@ -218,10 +272,14 @@ class Editor {
                 }
                 if (_event.type == sf::Event::MouseButtonPressed) {
                     if (_event.mouseButton.button == sf::Mouse::Left) {
-                        _toolbox.UpdateSelectedTileOnClick(window);
+                        if (_toolWindow.hasFocus()) {
+                            _toolbox.UpdateSelectedTileOnClick(window);
+                        }
                     }
-                    if (_event.mouseButton.button == sf::Mouse::Right) {
-                        ReplaceTileWithToolboxTile(window);
+                    if (_event.mouseButton.button == sf::Mouse::Left) {
+                        if (_mapWindow.hasFocus()) {
+                            ReplaceTileWithToolboxTile(window);
+                        }
                     }
                 }
             }
@@ -229,8 +287,11 @@ class Editor {
 
         void ReplaceTileWithToolboxTile(sf::RenderWindow &window)
         {
+            // Check if toolbox has a selected tile
             if (_toolbox.GetSelectedTile() == nullptr)
                 return;
+
+            // Get mouse position relative to map
             sf::Vector2i mousePos = sf::Mouse::getPosition(window);
             sf::Vector2f mapPos = _map->GetMapPosition();
             sf::Vector2f tilePos = sf::Vector2f(mousePos.x - mapPos.x, mousePos.y - mapPos.y);
@@ -239,14 +300,13 @@ class Editor {
             size_t y = tilePos.y / tileSize;
             Tile *tile = _toolbox.GetSelectedTile();
             std::cout << "Replacing tile at " << x << ", " << y << std::endl;
-            if (tile->GetType() == Tile::Type::WALL)
-                _map->GetLayers()[1]->SetTile(x, y, tile);
-            else if (tile->GetType() == Tile::Type::FLOOR)
-                _map->GetLayers()[0]->SetTile(x, y, tile);
-            else if (tile->GetType() == Tile::Type::OBJECT)
-                _map->GetLayers()[2]->SetTile(x, y, tile);
-            else
-                std::cerr << "Invalid tile type" << std::endl;
+
+            // Check if clicking out of bounds
+            if (x >= _map->GetLayers()[0]->GetWidth() || y >= _map->GetLayers()[0]->GetHeight())
+                return;
+
+            // Replace tile
+            _map->GetLayers()[tile->GetType()]->GetTile(x, y)->GetSprite()->setTextureRect(tile->GetSprite()->getTextureRect());
         }
 
         void Run()
